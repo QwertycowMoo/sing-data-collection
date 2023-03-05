afd0f518a0bd73436be78816f14379cccae0831b
Kevin Zhou <kevinzhou721@gmail.com>
2023-03-05 13:12:16 -0600

    hello world

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sing-data-collection
 Hosting a site to collect singing data for Senior Capstone Project
+
+### Emulating
+use `npm run serve` to build the Functions package and start the functions locally with the Firebase Emulator
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,13 @@ import * as functions from "firebase-functions";
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+export const audioData = functions.https.onRequest((req, res) => {
+    if (req.method == "GET"){
+        functions.logger.info("Request Recieved!")
+        res.status(200).send("A Ok")
+    } else if (req.method == "POST"){
+        functions.logger.info("Post recieved!")
+        res.status(200).send("A Ok")
+    }
+})
